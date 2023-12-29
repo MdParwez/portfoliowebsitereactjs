8690bd60622055d4ef0626b40e37c32c36db7718
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-29 23:17:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center mb-4 px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center mb-4 px-4 bg-black text-gray-300'>
       <div>
         <img src={Logo} alt='Logo' style={{ width: '50px' }} />
       </div>
