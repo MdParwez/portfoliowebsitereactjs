50b3d89ae5593a653a8466de01bc15b60d582edb
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-29 23:18:26 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,11 +14,11 @@ import Express  from '../assets/Express.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-black text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
-              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='text-4xl font-bold inline border-b-4 border-blue-600 '>Skills</p>
               <p className='py-4'></p>
           </div>
 
