e93931b75370b5878b82b06dd3cbcc5ba071776c
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-29 23:16:57 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,21 +48,21 @@ const Home = () => {
   };
 
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-black'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, My Name is</p>
+        <p className='text-blue-600'>Hi, My Name is</p>
         <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>
           Md Parwez
         </h1>
-        <h2 className='text-4xl sm:text-6xl font-bold text-pink-600'>
+        <h2 className='text-4xl sm:text-6xl font-bold text-blue-600'>
           {text}
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
         I absolutely love being a full-stack developer! My passion is creating awesome digital experiences. I focus on building websites that not only look great but also work flawlessly on any device. I'm all about blending design and development to make top-notch solutions. Always eager to learn and create something amazing in the world of web development!.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600'>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
@@ -75,3 +75,4 @@ const Home = () => {
 };
 
 export default Home;
+
