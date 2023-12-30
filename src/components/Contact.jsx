dec3809ce1f40a0cd9cebae565acd9adcea1fcbf
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-30 14:40:10 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
+  const inputBorderStyle = 'border-2 border-blue-600'; // Define the border style for inputs
+
   return (
     <div name='contact' className='w-full h-screen bg-black flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/496614c7-615b-48a5-bd6a-faaabae87f1f" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
-                <p className='text-blue-600 py-4'></p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Connect</button>
-        </form>
+      <form method='POST' action="https://getform.io/f/496614c7-615b-48a5-bd6a-faaabae87f1f" className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
+          <p className='text-blue-600 py-4'></p>
+        </div>
+        {/* Apply the border style to each form field */}
+        <input className={`bg-[#ccd6f6] p-2 ${inputBorderStyle}`} type="text" placeholder='Name' name='name' />
+        <input className={`my-4 p-2 bg-[#ccd6f6] ${inputBorderStyle}`} type="email" placeholder='Email' name='email' />
+        <textarea className={`bg-[#ccd6f6] p-2 ${inputBorderStyle}`} name="message" rows="10" placeholder='Message'></textarea>
+        <button className='text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Connect</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
 export default Contact;
+
