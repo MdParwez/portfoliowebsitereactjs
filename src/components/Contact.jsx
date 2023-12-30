be27a4813d5279d4aea24ceddb566b5bb46bde5d
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-30 13:15:30 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/496614c7-615b-48a5-bd6a-faaabae87f1f" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'></p>
+                <p className='text-blue-600 py-4'></p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
