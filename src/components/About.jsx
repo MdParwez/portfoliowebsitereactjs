5567db08e4c89d7165d64d370cec9aa7d633425f
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2023-12-31 15:16:33 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,8 +17,7 @@ const About = () => {
               <p>Hi. I'm Md Parwez, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate full Stack Developer Skilled in building Excellent software that improves
-              the lives of those around me. Whenever I encounter Any website i just try to improve their Features, Behaviour,User experiences of that website </p>  
+              <p>I'm a full-stack developer who loves making websites better! Whenever I come across a site, I can't resist tweaking things to improve how it works and feels for users. My goal is to create software that makes people's lives easier and more enjoyable. I'm all about enhancing features and experiences to give users something awesome to interact with! </p>  
             </div>
           </div>
       </div>
