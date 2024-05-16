e78437bc3fa4cd10748fa58b84c4cae2cf3ff017
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2024-05-17 01:33:36 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,31 +2,46 @@ import Weather from '../assets/projects/Weather.png'
 import project3 from  '../assets/projects/Project3.png'
 import portfolio from '../assets/projects/portfolio.png'
 import project1 from  '../assets/projects/Project1.png'
-
+import mern from '../assets/projects/mern.png'
+import onlinepayment from '../assets/projects/onlinepayment.png'
 export const data=[
+   {    id:1,
+        name:"Mern Messenger",
+        image:mern,
+        github:"https://github.com/MdParwez/mernchat?tab=readme-ov-file",
+        live:"https://mernchat-prod.onrender.com",
+    },
     {
-        id:1,
+     id:2,
+     name:"Payment Fraud Prediction",
+     image:onlinepayment,
+     github:"https://github.com/MdParwez/onlinepaymentfraud",
+     live:"https://tejasjd08-final-online-payment-fraud-detection.streamlit.app/",
+    }
+,
+    {
+        id:3,
         name:"Weather App ",
         image:Weather,
         github:"https://github.com/MdParwez/Weather-App",
         live:"https://mdparwez.github.io/Weather-App/",
     },
     {
-        id:2,
+        id:4,
         name:"portfolio Website",
         image:portfolio,
         github:"https://github.com/MdParwez/portfolio",
         live:" https://mdparwez.github.io/portfolio/",
     },
     {
-        id:3,
+        id:5,
         name:"Password generator",
         image:project3,
         github:"https://github.com/MdParwez/password-generator",
         live:"https://mdparwez.github.io/password-generator/",
     },
     {
-        id:4,
+        id:6,
         name:"Razorpay Clone",
         image:project1,
         github:"https://github.com/MdParwez/razorpayclone",
